76b9411c202960b2827b86e2ea7b2aed7f3b8e6f
Dilan Balci <dlanbalci@gmail.com>
2020-03-30 02:39:45 -0700

    Photo files added.

--- frontend/app.js
+++ frontend/app.js
@@ -4,7 +4,8 @@ class Game {
         this.time = 0;
         this.timer = document.getElementById("timer");
         this.timer.innerText = this.time + "s";
-        this.faces = ["😊", "😎", "🙄", "😘", "😑", "🤔", "😉", "🥺", "🤨", "😢", "🤐", "😵"];
+        this.happyFaces = ['h1.png', 'h2.png', 'h3.png', 'h4.png', 'h5.png', 'h6.png', 'h7.png', 'h8.png', 'h9.png', 'h10.png', 'h11.png', 'h12.png', 'h13.png'];
+        this.otherFaces = ['o1.png', 'o2.png', 'o3.png', 'o4.png', 'o5.png', 'o6.png', 'o7.png', 'o8.png', 'o9.png', 'o10.png', 'o11.png', 'o12.png', 'o13.png', 'o14.png', 'o15.png', 'o16.png', 'o17.png', 'o18.png', 'o19.png', 'o20.png', 'o21.png', 'o22.png', 'o23.png', 'o24.png', 'o25.png', 'o26.png'];
     }
 
     shuffle(arr) {
@@ -18,23 +19,33 @@ class Game {
         return arr;
     };
 
-    getRandomFaces() {
-        let newFace = this.shuffle(this.faces).splice(5);
-        newFace.push('😊','😊');
-        return this.shuffle(newFace);
-    };
+    getRandomFaces(){
+        let arr = [2, 3, 4];
+        let rnd = this.shuffle(arr)[0];
+        let newHappy = this.shuffle(this.happyFaces).slice(0,rnd)
+        let rest = document.querySelectorAll('.image').length - rnd;
+        let newOther= this.shuffle(this.otherFaces).slice(0,rest)
+        let newFaces = newHappy.concat(newOther);
+        return this.shuffle(newFaces)
+    }
 
     setPanel(){
         let randomFaces = this.getRandomFaces();
         let boxes = document.querySelectorAll('.image');
         for(let i=0; i<boxes.length; i++){
-            boxes[i].innerText = randomFaces[i];
+            let imgEl = document.createElement('img');
+            imgEl.src = '/faces/' + randomFaces[i];
+            boxes[i].innerHTML = '';
+            boxes[i].style.backgroundColor = '#f8e4dd';
+            boxes[i].appendChild(imgEl);
         };
     };
 
     start() {
         // start timer
         this.setPanel();
+        this.time = 0;
+
         let that = this;
         this.interval = setInterval(function () {
             that.time += 0.1;
@@ -57,28 +68,26 @@ class Game {
         this.rightFace = 0;
 
         for(let i=0; i<boxes.length; i++){
-            if (boxes[i].innerText === "😊") {
+            let imgName = boxes[i].querySelector('img').src.split('/').pop();
+
+            if (this.happyFaces.includes(imgName)) {
                 this.faceCounter += 1;
             };
-        };
-
-        for(let i=0; i<boxes.length; i++){
-
-        }
-        
+        };        
     };
 
     userClicked(e) {
-        let box = e.target;
-
+        let img = e.target;
+        let box = img.parentElement;
+        let imgName = img.src.split('/').pop();
         //check if it is  right face
-        if (box.innerText === "😊"){
+
+        if (this.happyFaces.includes(imgName)){
             box.style.backgroundColor = 'rgb(80, 155, 97)';
             this.rightFace+=1
         } else {
             box.style.backgroundColor = 'rgb(206, 62, 51)';
         }
-        
         if(this.rightFace === this.faceCounter){
             clearInterval(this.interval);
             document.getElementById('timer').style.color = 'white';
